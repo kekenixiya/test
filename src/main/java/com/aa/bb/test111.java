e70c56660181cde41da33afd4ef26b0fd095dd48
LIKEPING <290843745@qq.com>
2019-07-30 14:15:31 +0800

    Excel xsl 和 xslx 解析例子添加

--- src/main/java/com/aa/bb/test111.java
+++ src/main/java/com/aa/bb/test111.java
@@ -3,17 +3,19 @@ package com.aa.bb;
 public class test111 {
     public static void main(String[] args) {
 
-        String regax = "^[0-9]{1,2}$";
-        String str = "";
+//        String regax = "^[0-9]{1,2}$";
+//        String str = "";
 //        System.out.println(str.matches(regax));
 
 //        String area = "浙江省/杭州市/江干区";
-       String area = "浙江省/杭州市";
-        String areaaaaaa = area;
+//       String area = "浙江省/杭州市";
+//        String areaaaaaa = area;
+//
+//        System.out.println(area.contains("#"));
+//        int index = area.indexOf("#");
+//        int index2 = area.lastIndexOf("/");
+//        String  tempStr = areaaaaaa.substring(3);
 
-        System.out.println(area.contains("#"));
-        int index = area.indexOf("#");
-        int index2 = area.lastIndexOf("/");
-        String  tempStr = areaaaaaa.substring(3);
+        String str1 = "V1";
     }
 }
